679e05fd8eec3d09a3e70ea21bfa0712fc353895
Thom Treebus <thomtreebus@gmail.com>
2022-12-09 16:26:33 +0000

    modify start script to run express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  return console.log(
+    `⚡️[server]: Server is running at https://localhost:${port}`
+  );
+});

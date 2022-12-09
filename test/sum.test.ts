b6dc5a9f21f07f77de9a47a5697c5df5fe3c0481
Thom Treebus <thomtreebus@gmail.com>
2022-12-09 16:40:21 +0000

    trigger test workflow

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -3,3 +3,7 @@ const sumFunc = require("../src/sum");
 test("adds 1 + 2 to equal 3", () => {
   expect(sumFunc(1, 2)).toBe(3);
 });
+
+test("adds 2 + 2 to equal 4", () => {
+  expect(sumFunc(2, 2)).toBe(4);
+});

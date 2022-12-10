16be86ad7baf6b61e68cee085309ca7f6236377d
Thom Treebus <thomtreebus@gmail.com>
2022-12-10 19:20:43 +0000

    create ping endpoint to test server

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.get("/ping", (req: Request, res: Response) => {
+  res.json({ ping: "pong" });
+});
+
 app.listen(port, () => {
   return console.log(
     `⚡️[server]: Server is running at https://localhost:${port}`

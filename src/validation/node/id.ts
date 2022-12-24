7ec5ab2a8d9e8c598e48e4f1d5ef74a6233d04d4
Thom Treebus <thomtreebus@gmail.com>
2022-12-24 18:15:27 +0100

    validation for node id

--- src/validation/node/id.ts
+++ src/validation/node/id.ts
@@ -5,16 +5,14 @@ import { isNotString } from "../../utils/functions";
 
 export default (node: Node) => {
   if (isNil(node.id)) {
-    throw new Error(`The node id is required and must be a string.
-
-Node: ${toString(node)}`);
+    throw new Error(`The node id is required. Node: ${toString(node)}`);
   }
 
   if (isNotString(node.id)) {
-    throw new Error(`The node id must be a string.
-
-Node id type: ${type(node.id)}
-Node id: ${toString(node.id)}
-Node: ${toString(node)}`);
+    throw new Error(
+      `The node id must be a string. Node id type: ${type(
+        node.id
+      )}, Node id: ${toString(node.id)}, Node: ${toString(node)}`
+    );
   }
 };
---
+++ test/validation/node/id.test.ts
@@ -0,0 +1,38 @@
+import validNodeId from "../../../src/validation/node/id";
+
+describe("Node Id Validations", () => {
+  describe('When node does not have "id"', () => {
+    const node = { states: ["T", "F"] };
+
+    it("throws an error that node id is required", () => {
+      expect(() => {
+        // @ts-ignore
+        validNodeId(node);
+      }).toThrow(`The node id is required. Node: {"states": ["T", "F"]}`);
+    });
+  });
+
+  describe('When node has "id" but is not a string', () => {
+    const node = { id: 69 };
+
+    it("throws an error that node id must be a string", () => {
+      expect(() => {
+        // @ts-ignore
+        validNodeId(node);
+      }).toThrow(
+        `The node id must be a string. Node id type: Number, Node id: 69, Node: {"id": 69}`
+      );
+    });
+  });
+
+  describe('When node has "id" and is a string', () => {
+    const node = { id: "node-id" };
+
+    it("does not throws an error", () => {
+      expect(() => {
+        // @ts-ignore
+        validNodeId(node);
+      }).not.toThrow();
+    });
+  });
+});

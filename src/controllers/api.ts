51df4a3eb8a8aee346a4f2a5657fb2de98beb2c4
Thom Treebus <thomtreebus@gmail.com>
2022-12-29 16:04:46 +0100

    saving

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 
 module.exports.createNetwork = async (req: Request, res: Response) => {
   try {
-    const { hello } = req.body;
-    console.log(hello);
-    return res.status(200).json({ success: "create network" });
+    console.log(req.body);
+    return res.status(200).json({ success: "network successfully created" });
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);

ea06caa9b6429028dd1f8903a87eab6602534e17
Thom Treebus <thomtreebus@gmail.com>
2022-12-30 21:51:37 +0100

    delete middleware directory:

--- src/middleware/errorMiddleware.ts
+++
@@ -1,9 +0,0 @@
-module.exports.errorHandler = (err: any, req: any, res: any, next: any) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode);
-
-  res.json({
-    message: err.message,
-    stack: err.stack,
-  });
-};

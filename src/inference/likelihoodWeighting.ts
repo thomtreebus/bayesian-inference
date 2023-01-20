d42d68b2c52eadf58d5203530b43c4ae59bf9518
Thom Treebus <thomtreebus@gmail.com>
2023-01-20 14:45:54 +0000

    idk what i'm doing

--- src/inference/likelihoodWeighting.ts
+++ src/inference/likelihoodWeighting.ts
@@ -7,12 +7,26 @@ import {
   Combinations,
 } from "../types";
 
+function weightedSample(network: Network, observedValues?: Combinations) {
+  const x = observedValues;
+  const w = 1;
+
+  return {
+    event: x,
+    weigh: w,
+  };
+}
+
 function likelihoodWeighting(
   network: Network,
   queryNodes: Combinations,
   sampleSize: number,
   observedValues?: Combinations
 ): number {
+  const W: number[] = [];
+  W.push(0);
+  console.log(W);
+
   return 0;
 }
 

81872567045713630c30815cd7825e2578c9afd8
Thom Treebus <thomtreebus@gmail.com>
2023-02-02 14:30:19 +0000

    begin work on evaluator

--- src/evaluation/evaluator.ts
+++ src/evaluation/evaluator.ts
@@ -2,10 +2,10 @@ import { Infer } from "../../src/types";
 import { allNodes } from "../../networks/alarm";
 import { createNetwork } from "../../src/utils/network";
 import { inferenceAlgorithms } from "../../src";
-const reader = require("xlsx");
-// const file = reader.readFile("./output.xlsx");
-
 const { likelihoodWeighting } = inferenceAlgorithms;
+const { convertArrayToCSV } = require("convert-array-to-csv");
+const converter = require("convert-array-to-csv");
+const fs = require("fs");
 
 const network = createNetwork(...allNodes);
 const sampleSize = 10000;
@@ -16,17 +16,30 @@ console.log("evaluating....");
 // ];
 const results = [];
 const sampleSizes = [10, 100, 500, 1000];
-for (let sampleSize = 1; sampleSize < 10; sampleSize++) {
+for (let sampleSize = 1; sampleSize < 10000; sampleSize++) {
   const observedValues = { BURGLARY: "T" };
-  results.push(
+  results.push([
+    sampleSize,
     likelihoodWeighting
       .infer(network, { MARY_CALLS: "T" }, observedValues, sampleSize)
-      .toFixed(4)
-  );
+      .toFixed(4),
+  ]);
 }
 
 console.log("finished!");
-// console.log(results);
+console.log(results);
 
-var csv = results.join(",");
-console.log(csv);
+const header = ["Sample Size", "Output"];
+const csv = convertArrayToCSV(results, {
+  header,
+  seperator: ";",
+});
+// console.log(csv);
+
+try {
+  fs.writeFileSync("test.csv", csv);
+  console.log("file written successfully!");
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}

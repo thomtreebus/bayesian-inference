dbd37d815e7729306f91fe905005a8f2d2da0d5e
Thom Treebus <thomtreebus@gmail.com>
2023-03-06 16:19:20 +0000

    create var elimination file

---
+++ src/inference/elimination.ts
@@ -0,0 +1,17 @@
+import {
+  Node,
+  Network,
+  CptWithParents,
+  CptWithoutParents,
+  Infer,
+  Combinations,
+} from "../types";
+
+export const infer: Infer = (
+  network: Network,
+  query: Combinations = {},
+  observedValues: Combinations,
+  sampleSize: number = 0
+) => {
+  return 0;
+};

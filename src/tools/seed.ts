9d40f9d60ebc08f5d7856c3848149486b1ef8e45
Thom Treebus <thomtreebus@gmail.com>
2023-04-05 12:56:23 +0100

    use env for seed/unseed commands

--- src/tools/seed.ts
+++ src/tools/seed.ts
@@ -1,10 +1,14 @@
 import { seed } from "./seeder";
 import { unseed } from "./unseeder";
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const uri: string = process.env.DB_CONNECTION_URL as string;
+console.log("url", uri);
 /**
  * Connect to the database and run the seeder.
  */
-mongoose.connect("mongodb://localhost:27017/db", async () => {
+mongoose.connect(uri, async () => {
   console.log("Connected to the database!");
   await unseed(); // Clear the entire database
   await seed(); // Seed the database
--- src/tools/unseed.ts
+++ src/tools/unseed.ts
@@ -1,10 +1,12 @@
 const { unseed } = require("./unseeder");
 const mongooses = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const uri: string = process.env.DB_CONNECTION_URL as string;
 /**
  * Connect to the database and unseed it
  */
-mongooses.connect("mongodb://localhost:27017/db", async () => {
+mongooses.connect(uri, async () => {
   console.log("Connected to the database!");
   await unseed(); // Unseed the database
   process.exit();
